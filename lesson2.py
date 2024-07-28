d484bb0523c86bbaef47564e9f1f6535aa26e345
Fukyrdrs <artm.filipppov@mail.ru>
2024-07-28 12:17:53 +0300

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,5 @@
+task = 12
+hours = 1.5
+name = 'Python'
+task_and_hours = hours/task
+print('Курс:', name,', всего задач:', task,', затрачено часов:', hours,', среднее время выполнения:', task_and_hours)
